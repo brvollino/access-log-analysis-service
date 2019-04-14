0d8ef69f83edb6f81f690816ac9218fc428d3985
Bruno <brvollino@gmail.com>
2019-04-14 00:16:14 -0300

    Fixes pre-condition

--- src/main/java/com/vollino/log/analyser/Application.java
+++ src/main/java/com/vollino/log/analyser/Application.java
@@ -9,7 +9,7 @@ import com.vollino.log.analyser.server.JettyServer;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        Preconditions.checkArgument(args.length == 0, "The server port must be provided as an argument");
+        Preconditions.checkArgument(args.length >= 1, "The server port must be provided as an argument");
         JettyServer server = new JettyServer(Integer.parseInt(args[0]));
         server.start();
         server.join();
